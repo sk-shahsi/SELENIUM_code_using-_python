f966b1bc613d8cd1a215a59f71705f6a817ce83b
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-01 19:52:24 +0530

    google_search

--- sel_1.py
+++ sel_1.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
+
+options = webdriver.ChromeOptions()
+
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.com")
 
 
-time.sleep(10)
+
+search = driver.find_element(By.NAME, "q")
+search.send_keys("selenium python")
+time.sleep(3)
+search.submit()
+time.sleep(50)
+
+
+# button = driver.find_element(By.NAME, "btnK")
+# button.click()
