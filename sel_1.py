a3b2bfc86cd65bec5051ee482c97d74393f0ef49
sk-shahsi <sk.sashikumar055@gmail.com>
2025-12-01 21:44:11 +0530

    google_search

--- sel_1.py
+++ sel_1.py
@@ -1,21 +1,23 @@
-from selenium import webdriver
+
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 import time
 
-options = webdriver.ChromeOptions()
+driver = uc.Chrome()
 
-
-driver = webdriver.Chrome()
 driver.get("https://www.google.com")
-
-
 
 search = driver.find_element(By.NAME, "q")
 search.send_keys("selenium python")
-time.sleep(3)
+
+time.sleep(5)  # wait 5 seconds
 search.submit()
+
+time.sleep(10)
+
+driver.back()
+time.sleep(5)
+driver.forward()
 time.sleep(50)
 
 
-# button = driver.find_element(By.NAME, "btnK")
-# button.click()
